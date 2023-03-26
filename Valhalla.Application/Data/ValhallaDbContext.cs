e323a8b9c6e1001787c662de65ef596e5bd3e457
Ranjbar, AmirAbbas (TEK) <ranjbar.amirabbas@okala.com>
2023-03-26 12:31:17 +0330

    fix context

--- Valhalla.Application/Data/ValhallaDbContext.cs
+++ Valhalla.Application/Data/ValhallaDbContext.cs
@@ -5,19 +5,18 @@ namespace Valhalla.Application.Data;
 
 public class ValhallaDbContext : DbContext
 {
-    public const string DefaultSchema = "Valhalla";
+    public DbSet<Partners> Partners { get; set; }
 
   
     public ValhallaDbContext(DbContextOptions options) : base(options)
     {
     }
 
-    public DbSet<Partners> Partners { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
-        modelBuilder.HasDefaultSchema(DefaultSchema);
+        modelBuilder.Entity<Partners>().ToTable(nameof(Partners), t => t.ExcludeFromMigrations());
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ValhallaDbContext).Assembly);
     }
 }
